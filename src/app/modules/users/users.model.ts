c18b888dee939c43f78608c2a9b00a9eaa78851a
rahat-495 <md8702687@gmail.com>
2025-02-20 21:05:59 +0600

    user interfaces or model are done !

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,45 @@
+
+import { model, Schema } from "mongoose";
+import { TUser } from "./users.interfaces";
+import bcrypt from 'bcrypt' ;
+import config from "../../config";
+
+const userSchema = new Schema<TUser>({
+    name : {
+        type : String ,
+        required : true ,
+    },
+    email : {
+        type : String ,
+        unique : true ,
+        required : true ,
+    },
+    password : {
+        type : String ,
+        required : true ,
+        select : 0 ,
+    },
+    phone : {
+        type : Number ,
+        required : true ,
+    },
+    role : {
+        type : String ,
+        default : "user"
+    },
+} , {
+    timestamps : true ,
+})
+
+userSchema.pre("save" , async function(next){
+    const user = this ;
+    user.password = await bcrypt.hash(user?.password , Number(config.bcryptSaltRounds)) ;
+    next() ;
+})
+
+userSchema.post("save" , function(doc , next){
+    doc.password = "" ;
+    next() ;
+})
+
+export const usersModel = model<TUser>("user" , userSchema) ;
